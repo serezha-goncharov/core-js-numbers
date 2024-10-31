dfd00d48638f1bcd42bd12689f2a5947dd08f7b4
Sergei Goncharov <blist.zheleza@gmail.com>
2024-10-31 17:55:12 +0700

    feat: solve getSumToN subtask

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -296,8 +296,12 @@ function getFibonacciNumber(index) {
  *   10 => 55 // (1+2+3+...+10)
  *   1  => 1
  */
-function getSumToN(/* n */) {
-  throw new Error('Not implemented');
+function getSumToN(n) {
+  let result = 0;
+  for (let i = 1; i <= n; i += 1) {
+    result += i;
+  }
+  return result;
 }
 
 /**
