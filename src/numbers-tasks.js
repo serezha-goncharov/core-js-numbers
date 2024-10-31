c13c5c475eb55067af80bdb2d54565313fe7f82c
Sergei Goncharov <blist.zheleza@gmail.com>
2024-10-31 17:52:30 +0700

    feat: solve roundToPowerOfTen subtask

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -180,8 +180,9 @@ function getParallelepipedDiagonal(a, b, c) {
  *   1678, 2  => 1700
  *   1678, 3  => 2000
  */
-function roundToPowerOfTen(/* num, pow */) {
-  throw new Error('Not implemented');
+function roundToPowerOfTen(num, pow) {
+  const k = 10 ** pow;
+  return Math.round(num / k) * k;
 }
 
 /**
